ed4a526582c00245dfe3f24232d749990e40820f
makul <marcin.kulisiewicz@pwr.edu.pl>
2017-11-30 23:26:00 -0500

    news_box v1.1

--- hallelujah/js/hhcustom.js
+++ hallelujah/js/hhcustom.js
@@ -1,5 +1,15 @@
+function newsBoxTimer() {
+    setTimeout(showNewsBox,2000);
+}
+
+function showNewsBox() {
+    obNewsBox = document.getElementById("news-box-1");
+    obNewsBox.style.visibility = "visible";
+}
+
 function closeNewsBox() {
     obNewsBox = document.getElementById("news-box-1");
     obNewsBox.style.visibility = "hidden";
-//    obNewsBox.style.display = "none";
 }
+
+window.onload = newsBoxTimer();
